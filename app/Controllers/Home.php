39ca2410af120fdf88ad96a040b1ccc3337f0808
Crateink <crateink@gmail.com>
2025-03-02 01:36:48 +0800

    feat: 定义表结构

--- app/Controllers/Home.php
+++ app/Controllers/Home.php
@@ -7,7 +7,6 @@ class Home extends BaseController
     public function index(): string
     {
         $user = auth()->user();
-
         return view('index');
     }
 }
---
+++ app/Database/Migrations/2025-03-01-163055_CreateTable.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Database\Migrations;
+
+use CodeIgniter\Database\Migration;
+
+// php spark make:migration	创建迁移文件
+// php spark migrate	运行所有未应用的迁移
+// php spark migrate:rollback	回滚上一次迁移
+// php spark migrate:refresh	回滚所有迁移并重新运行
+// php spark migrate:status	查看迁移状态
+// php spark make:seed	创建种子文件
+// php spark db:seed	运行种子文件
+class CreateTable extends Migration
+{
+    public function up(): void
+    {
+        // 定义表结构
+        $this->forge->addField([
+            'id'         => ['type' => 'int', 'constraint' => 11, 'unsigned' => true, 'auto_increment' => true],
+            'name'       => ['type' => 'varchar', 'constraint' => 30, 'null' => true],
+            'extra'      => ['type' => 'text', 'null' => true],
+            'image'      => ['type' => 'text', 'null' => true],
+            'created_at' => ['type' => 'datetime', 'null' => true],
+            'updated_at' => ['type' => 'datetime', 'null' => true],
+            'deleted_at' => ['type' => 'datetime', 'null' => true],
+        ]);
+        $this->forge->addPrimaryKey('id');
+        $this->forge->createTable('games');
+    }
+
+    public function down()
+    {
+        // 回滚操作：删除表
+        $this->forge->dropTable('games');
+    }
+}
